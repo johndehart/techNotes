337f27672f06ad56998de4a32d51fe46afcb3db1
John K. DeHart <jdehart@avian.com>
2023-09-20 11:31:51 -0400

    Initial commit

---
+++ .jupyter/jupyter_notebook_config.py
@@ -0,0 +1,5 @@
+c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
+c.ContentsManager.default_jupytext_formats = "ipynb,md"
+c.ContentsManager.synchronized_save = True
+c.ContentsManager.default_notebook_metadata_filter = "all"
+c.ContentsManager.default_cell_metadata_filter = "all"
